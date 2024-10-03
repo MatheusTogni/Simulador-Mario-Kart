fcd1bb419def02927e2e282174a9a35ab498f4fd
Matheus Togni <matheustogni64@gmail.com.br>
2024-10-03 11:22:25 -0300

    quase terminando

--- src/index.js
+++ src/index.js
@@ -1,37 +1,43 @@
 let Mario = {
     velocidade: 4,
     manobrabilidade: 3,
-    poder: 3
+    poder: 3,
+    pontos: 0
 }
 
 let Peach = {
     velocidade: 3,
     manobrabilidade: 4,
-    poder: 2
+    poder: 2,
+    pontos: 0
 }
 
 let Yoshi = {
     velocidade: 2,
     manobrabilidade: 4,
-    poder: 3
+    poder: 3,
+    pontos: 0
 }
 
 let Bowser = {
     velocidade: 5,
     manobrabilidade: 2,
-    poder: 5
+    poder: 5,
+    pontos: 0
 }
 
 let Luigi = {
     velocidade: 3,
     manobrabilidade: 4,
-    poder: 4
+    poder: 4,
+    pontos: 0
 }
 
 let DonkeyKong = {
     velocidade: 2,
     manobrabilidade: 2,
-    poder: 5
+    poder: 5,
+    pontos: 0
 }
 
 let jogador1 = DonkeyKong
@@ -52,9 +58,33 @@ for (let i = 0; i < 5; i++) {
     switch (numeroAleatorio) {
         case 1:
             console.log('Pista estilo RETA');
+            jogador1.velocidade = jogador1.velocidade + sorteiaDado()
+            jogador2.velocidade = jogador2.velocidade + sorteiaDado()
+
+            if (jogador1.velocidade > jogador2.velocidade) {
+                console.log('Jogador 1 VENCEU, Velocidade jogador 1:', jogador1.velocidade, 'Velocidade jogador 2:', jogador2.velocidade)
+                jogador1.pontos = +1
+            } else if (jogador2.velocidade > jogador1.velocidade) {
+                jogador2.pontos = +1
+                console.log('Jogador 2 VENCEU, Velocidade jogador 1:', jogador1.velocidade, 'Velocidade jogador 2:', jogador2.velocidade)
+            } else {
+                console.log('Empatado, ninguém ganha PONTOS!')
+            }
             break;
         case 2:
             console.log('Pista estilo CURVA');
+            jogador1.manobrabilidade = jogador1.manobrabilidade + sorteiaDado()
+            jogador2.manobrabilidade = jogador2.manobrabilidade + sorteiaDado()
+
+            if (jogador1.manobrabilidade > jogador2.manobrabilidade) {
+                console.log('Jogador 1 VENCEU, Manobrabilidade jogador 1:', jogador1.manobrabilidade, 'Manobrabilidade jogador 2:', jogador2.manobrabilidade)
+                jogador1.pontos = +1
+            } else if (jogador2.manobrabilidade > jogador1.manobrabilidade) {
+                jogador2.pontos = +1
+                console.log('Jogador 2 VENCEU, Manobrabilidade jogador 1:', jogador1.manobrabilidade, 'Manobrabilidade jogador 2:', jogador2.manobrabilidade)
+            } else {
+                console.log('Empatado, ninguém ganha PONTOS!')
+            }
             break;
         case 3:
             console.log('Pista estilo CONFRONTO');
